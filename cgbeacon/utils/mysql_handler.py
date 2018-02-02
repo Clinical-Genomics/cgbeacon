6b761e2cab883a602dff0030a3fc52f153b43f16
northwestwitch <rasi.chiara@gmail.com>
2018-02-02 17:39:40 +0100

    small fix

--- cgbeacon/utils/mysql_handler.py
+++ cgbeacon/utils/mysql_handler.py
@@ -63,7 +63,7 @@ def remove_variants(conn, dataset, list_of_var_tuples):
 
     for var_tuple in list_of_var_tuples:
         try:
-            unique_key = dataset+"_"+str(val[0])+"_"+str(val[1])+"_"+val[2]
+            unique_key = dataset+"_"+str(var_tuple[0])+"_"+str(var_tuple[1])+"_"+var_tuple[2]
             # Remove 1 from the occurrence field if this is not the last occurrence
             sql = "update beacon_data_table set occurrence = occurrence -1 where chr_pos_alt_dset='"+unique_key+"'"
             result = conn.execute(sql, var_tuple[1], var_tuple[0], var_tuple[2])
