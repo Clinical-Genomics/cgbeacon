2f4967cf3efc597151b1dd34d291bcaa83f37d5a
northwestwitch <rasi.chiara@gmail.com>
2018-03-09 15:29:18 +0100

    revert utility code because bug is fixed elsewhere

--- cgbeacon/utils/Utility.py
+++ cgbeacon/utils/Utility.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from cyvcf2 import VCF
-from cgbeacon.utils.mysql_handler import bare_variants_uploader, remove_variants
+from cgbeacon.utils.mysql_handler import bare_variants_uploader, remove_variants, variants_per_dataset, update_dataset_vars
 from cgbeacon.utils.vcf_panel_filter import vcf_intersect
 from cgbeacon.utils.vcfparser import get_variants, count_variants
 from cgbeacon.utils.pdf_report_writer import create_report
@@ -16,20 +16,22 @@ def beacon_clean(connection, sample, vcf_path, panel_path=None, qual=20):
 
         Returns: number of new variants removed from the beacon for this sample.
     """
+    print("Counting raw variants..")
     raw_variants = count_variants(vcf_path)
     vcf_results = None
     panel_filtered_results = None
     # Filter original VCF file for regions in gene panels:
     if panel_path:
         panel_filtered_results = vcf_intersect(vcf_path, panel_path)
-        vcf_results = get_variants(panel_filtered_results[0], panel_filtered_results[0].raw_variants, [sample], qual)
+        vcf_results = get_variants(panel_filtered_results[0], panel_filtered_results[2], [sample], qual)
 
     else: #No filtering by panel:
         vcf_obj = VCF(vcf_path)
-        vcf_results = get_variants(vcf_obj, vcf_obj.raw_variants, [sample], qual)
+        vcf_results = get_variants(vcf_obj, raw_variants, [sample], qual)
 
     #Do the actual variant removal:
-    removed = remove_variants(connection, vcf_results[1][sample])
+    removed = remove_variants(connection, 'clinicalgenomics', vcf_results[1][sample])
+    dataset_vars = update_dataset_vars(connection, 'clinicalgenomics', variants_per_dataset(connection, 'clinicalgenomics'))
     return removed
 
 def beacon_upload(connection, vcf_path, panel_path, dataset, outfile=None, customer="", samples=None, qual=20, genome_reference="grch37"):
@@ -55,16 +57,20 @@ def beacon_upload(connection, vcf_path, panel_path, dataset, outfile=None, custo
     # the number of original intervals in the bed panel
     # the number of variants mapping to these intervals
 
+    # Get number of raw variants in original VCF file:
+    print("Counting raw variants..")
+    raw_variants = count_variants(vcf_path)
+
     # If the vcf should be filtered by a gene panel bed file:
     panel_filtered_results = None
     vcf_results = None
     if panel_path:
         panel_filtered_results = vcf_intersect(vcf_path, panel_path)
-        vcf_results = get_variants(panel_filtered_results[0], panel_filtered_results[0].raw_variants, samples, qual)
+        vcf_results = get_variants(panel_filtered_results[0], panel_filtered_results[2], samples, qual)
 
     else:
         vcf_obj = VCF(vcf_path)
-        vcf_results = get_variants(vcf_obj, vcf_obj.raw_variants, samples, qual)
+        vcf_results = get_variants(vcf_obj, raw_variants, samples, qual)
 
     # get_variants() returns a this tuple-> ( n_total_vars, beacon_vars(type: dict), discaded_vars(type: dict))
     ### beacon_vars is a disctionary with key --> sample, and value --> list of tuples containing the non-reference variants. Each tuple is defined as: (chr, start, alt_allele)
@@ -73,11 +79,11 @@ def beacon_upload(connection, vcf_path, panel_path, dataset, outfile=None, custo
 
     # Insert variants into the beacon. It returns a tuple: (vars_before_upload, vars_after_upload)
     beacon_update_result = bare_variants_uploader(connection, dataset, vcf_results, genome_reference)
+    dataset_vars = update_dataset_vars(connection, 'clinicalgenomics', variants_per_dataset(connection, 'clinicalgenomics'))
 
     # Print the pdf report with the variant upload results:
     if outfile:
-        # Get number of raw variants in original VCF file:
-        raw_variants = count_variants(vcf_path)
+
         print("Printing a report with beacon upload results to --> ",outfile)
         title = "Clinical Genomics Beacon: variants upload report"
         create_report(title, outfile, panel_path, raw_variants, qual, vcf_results, beacon_update_result, customer)
